1663efb6a152d4f2d1ac70870e79f40c6ece6d50
kleshas <0kleshas0@gmail.com>
2020-09-09 16:02:29 -0700

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,1 @@
+rsync -avP --delete --exclude=Proton* --exclude=cache --exclude=.cache --exclude=.cargo --exclude=appcache --exclude=Cache --exclude=Cities_Skylines /home/bhava/ /mnt/veracrypt3/home\ backup
