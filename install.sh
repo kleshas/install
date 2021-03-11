286d46251b90e2c1647b06943cb0ffac7e0ac802
kleshas <kleshas@mailbox.org>
2021-03-10 18:32:28 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,7 +55,6 @@
 	echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
 	echo "kernel.dmesg_restrict = 1" | sudo tee -a /etc/sysctl.d/50-dmesg-restrict.conf
 	echo "Storage=none" |sudo tee -a /etc/systemd/coredump.conf
-	sudo systemctl daemon-reload
 	echo "--country 'United States' --protocol https --age 12 --sort rate --latest 5 --save /etc/pacman.d/mirrorlist
 " |sudo tee /etc/xdg/reflector/reflector.conf
 	sudo systemctl enable reflector.service
