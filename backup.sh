cc584c9ab8fa5a4726cf0dba5e216c7d639aff35
kleshas <kleshas@mailbox.org>
2021-06-02 21:53:29 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,2 @@
 rsync -avP --delete --exclude=Proton* --exclude=cache --exclude=.cache --exclude=.cargo --exclude=appcache --exclude=Cache --exclude=Cities_Skylines /home/bhava/ /mnt/veracrypt3/home\ backup
+read -p "Press any key to exit ..."
