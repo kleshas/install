d68b4620f17f4006dc9f9ae5955967c8809ffaf4
kleshas <kleshas@mailbox.org>
2021-06-14 17:06:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,6 @@
 	mkdir ~/.chatty
 	mv ~/.config/chatty/settings ~/.chatty/
 	rm -f ~/.config/chatty
-	chmod +x ~/.config/i3/pipewire-pulse
 	rm ~/.bashrc
 	cp ~/.dotfiles/.bashrc ~/
 	cp ~/.dotfiles/.Xresources ~/
