b80049ec112a1441f67881964ae5125e2c6a0168
kleshas <kleshas@mailbox.org>
2024-07-29 21:02:00 -0700

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -117,7 +117,4 @@ EOF
 
 echo "options cryptdevice=PARTUUID=$(blkid -s PARTUUID -o value /dev/${target}p2):root:allow-discards root=/dev/mapper/root rw quiet split_lock_detect=off loglevel=3 ibt=off" >> /mnt/boot/loader/entries/arch.conf
 
-arch-chroot -u $username /mnt mkdir /home/$username/.dotfiles
-arch-chroot /mnt pacman -S git
-
 echo "Reboot, log in as $username and run bash ~/.dotfiles/.scripts/install.sh
