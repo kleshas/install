b36e16d255cf4422c36e50fa0e081efc18a161bc
kleshas <kleshas@mailbox.org>
2024-08-02 19:01:36 -0700

    Update baseinstall.sh

--- baseinstall.sh
+++ baseinstall.sh
@@ -14,9 +14,10 @@ locale="en_CA.UTF-8"
 hostname=$(date +%Y%b)
 
 # Partition
+echo -e "\e[1;31mCreating partitions...\n\e[0m"
 lsblk
-read -p "what drive to install to? " target
-echo "Creating partitions..."
+read -p "\e[1;31mWhat drive to install to?   \e[0m" target
+echo "\e[1;31mCreating partitions...\e[0m"
 #sgdisk -Z /dev/$target
 sgdisk -d 1 -d 2 /dev/$target
 sgdisk \
@@ -28,31 +29,32 @@ sgdisk \
 sleep 2
 partprobe -s /dev/$target
 sleep 2
-echo "Encrypting root partition..."final
+echo "\e[1;31mEncrypting root partition...\e[0m"final
 
 #Encrypt the root partition. prompt for crypt password
+echo -e "\e[1;31mEncrypting the root partition...\e[0m\n"
 cryptsetup luksFormat /dev/disk/by-partlabel/linux
 cryptsetup luksOpen /dev/disk/by-partlabel/linux root
 
-echo "Making File Systems..."
+echo -e "\e[1;31mMaking File Systems...\e[0m\n"
 # Create file systems
 mkfs.fat -F32 -n EFISYSTEM /dev/disk/by-partlabel/boot
 mkfs.ext4 -L linux /dev/mapper/root
 
 # mount the root, and create + mount the EFI directory
-echo "Mounting File Systems..."
+echo -e "\e[1;31mMounting File Systems...\e[0m\n"
 mount /dev/mapper/root /mnt
 mkdir /mnt/boot
 mount /dev/disk/by-partlabel/boot /mnt/boot
 
 #Update pacman mirrors and then pacstrap base install
-echo "Pacstrapping..."
+echo -e "\e[1;31mPacstrapping...\e[0m\n"
 pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode nano cryptsetup git
 genfstab -pU /mnt >> /mnt/etc/fstab
 #Decrease writes to the USB by using the noatime option in fstab
 sed -i 's/relatime/noatime/' /mnt/etc/fstab
 
-echo "Setting up environment..."
+echo -e "Setting up environment...\e[0m\n"
 echo $hostname > /mnt/etc/hostname
 arch-chroot /mnt hwclock --systohc --utc
 #set up locale/env
@@ -62,12 +64,13 @@ arch-chroot /mnt locale-gen
 echo LANG=${locale} > /mnt/etc/locale.conf
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Vancouver /etc/localtime
 
-echo "Configuring for first boot..."
+echo "\e[1;31mConfiguring for first boot...\e[0m"
 #add the local user
-echo -e "[ * ]Adding user"
-read -p "Username " username
+echo -e "\e[1;31m[ * ]Adding user\e[0m\n"
+read -p "\e[1;31mUsername \e[0m" username
 arch-chroot /mnt useradd -mG wheel $username
 arch-chroot /mnt passwd $username
+echo -e "\e[1;31mChange the root password...\e[0m\n"
 arch-chroot /mnt passwd root
 
 #uncomment the wheel group in the sudoers file
@@ -79,7 +82,7 @@ sed -i 's/keymap/keymap encrypt/g' /mnt/etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -p linux
  
 #enable the services we will need on start up
-echo "Enabling services..."
+echo -e "\e[1;31mEnabling services...\e[0m\n"
 systemctl --root /mnt enable systemd-resolved systemd-networkd
 cat <<EOF > /mnt/etc/systemd/network/20-wired.network
 	[Match]
@@ -123,8 +126,9 @@ cat <<EOF > /mnt/boot/loader/entries/arch.conf
 EOF
 echo "options cryptdevice=PARTUUID=$(blkid -s PARTUUID -o value /dev/${target}p2):root:allow-discards root=/dev/mapper/root rw quiet split_lock_detect=off loglevel=3 ibt=off" >> /mnt/boot/loader/entries/arch.conf
 
+echo -e "\e[1;31mDownload dotfiles...\e[0m\n"
 arch-chroot /mnt su $username <<EOF
 	mkdir ~/.dotfiles
 	git clone https://gitlab.com/kleshas/dots.git ~/.dotfiles
 EOF
-echo "Reboot, log in as $username and run bash ~/.dotfiles/.scripts/install.sh"
+echo -e "\n\n\n\e[1;31mReboot, log in as $username and run bash ~/.dotfiles/.scripts/install.sh\e[0m"
